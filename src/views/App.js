e8cfb420f003a0baec711e355171701890be9568
quangnd512 <quangnd512@gmail.com>
2022-03-08 06:23:14 +0700

    React - Update routers

--- src/views/App.js
+++ src/views/App.js
@@ -10,59 +10,59 @@ import Home from './Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // react-router-dom 6.x.x
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-      <Nav />
-      <header className="App-header">
-        <h1>Todo List</h1>
+    return ( 
+		<Router >
+        <div className = "App" >
+        <Nav />
+        <header className = "App-header" >
+        <h1 > Todo List </h1>
 
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+        {
+            /* <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React
+                    </a> */
+        } 
+		</header>
 
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-        {/* <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/todo">
-            <ListTodo />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch> */}  {/*react-router-dom 5.x.x*/}
+        <ToastContainer position = "top-right"
+        autoClose = { 5000 }
+        hideProgressBar = { false }
+        newestOnTop = { false }
+        closeOnClick rtl = { false }
+        pauseOnFocusLoss draggable pauseOnHover /> 
+		{
+            /* <Switch>
+                      <Route path="/about">
+                        <About />
+                      </Route>
+                      <Route path="/todo">
+                        <ListTodo />
+                      </Route>
+                      <Route path="/">
+                        <Home />
+                      </Route>
+                    </Switch> */
+        } { /*react-router-dom 5.x.x*/ }
 
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/todo" element={<ListTodo/>}/>
-          {/* <Route exact path="/about" element={}/> */}
-        </Routes>
-        {/*react-router-dom 6.x.x*/}
-    </div>
-    </Router>
-  );
+        <Routes >
+        <Route exact path = "/"
+        element = { < Home /> }/> 
+		<Route exact path = "/todo"
+        element = { < ListTodo /> }/> { 
+		/* <Route exact path="/about" element={}/> */ } 
+		</Routes> { /*react-router-dom 6.x.x*/ } 
+		</div> 
+		</Router>
+    );
 }
 
 export default App;
--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -2,24 +2,28 @@ import React from "react";
 import '../../styles/Nav/nav.scss'
 import { Link, NavLink } from "react-router-dom";
 
-class Nav extends React.Component
-{
-    render(){
-        return(
-            <div className="topnav">
-                <NavLink end to="/" activeClassName="active">
-                    Home
-                </NavLink>
-                <NavLink end to="/todo" activeClassName="active">
-                    Todo
-                </NavLink>
-                <NavLink end to="/about" activeClassName="active">
-                    About
-                </NavLink>
-                {/* <Link to="/">Home</Link> |{" "}
-                <Link to="/todo">Todo</Link>
-                <Link to="/about">About</Link> */}
-            </div>
+class Nav extends React.Component {
+    render() {
+        return ( <
+            div className = "topnav" >
+            <
+            NavLink end to = "/"
+            activeClassName = "active" >
+            Home <
+            /NavLink> <
+            NavLink end to = "/todo"
+            activeClassName = "active" >
+            Todo <
+            /NavLink> <
+            NavLink end to = "/about"
+            activeClassName = "active" >
+            About <
+            /NavLink> {
+                /* <Link to="/">Home</Link> |{" "}
+                                <Link to="/todo">Todo</Link>
+                                <Link to="/about">About</Link> */
+            } <
+            /div>
         )
     }
 }
